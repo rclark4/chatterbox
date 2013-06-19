ff78d767522972e7a63269e1a14e1996ac262e9b
Ryan Clark <itsryanclark@gmail.com>
2013-06-19 14:50:15 -0500

    added doodads like last post in words and by whom

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -2,4 +2,5 @@ class Forum < ActiveRecord::Base
   attr_accessible :description, :title
   has_many :topics, :dependent => :destroy
   has_many :posts, :through => :topics
+  
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,9 @@ class Topic < ActiveRecord::Base
   attr_accessible :content, :forum_id, :title
   belongs_to :forum
   has_many :posts
+
+  def most_recent_post  
+  	post = Post.first(:order => 'last_post_at DESC', :conditions => ['forum_id = ?', self.id])  
+  	return post
+	end  
 end
